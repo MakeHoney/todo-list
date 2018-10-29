5b42362a260657f7fc5e807b1e91c831685f2761
makehoney <makehoney3@gmail.com>
2018-10-29 21:42:44 +0900

    add basic sign-in form

--- client/src/components/SignIn.vue
+++ client/src/components/SignIn.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
-
+        <h2>Sign In</h2>
+        <form @submit.prevent="onSubmit(uid, password)">
+            <input type="text" v-model="uid" placeholder="아이디">
+            <input type="password" v-model="password" placeholder="비밀번호">
+            <input type="submit" value="로그인">
+        </form>
     </div>
 </template>
 <script>
 export default {
-    name: 'SignIn'
+    name: 'SignIn',
+    data () {
+        return {
+            uid: '',
+            password: ''
+        }
+    },
+    methods: {
+        async onSubmit (uid, password) {
+            await this.$store.dispatch('signIn', { uid, password })
+            // redirect ?
+            // this.redirect()
+            this.$router.push('home')
+        }
+    }
 }
 </script>
 
