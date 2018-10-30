80947983c7e0717ff8280ed603e922ecad7024d6
makehoney <makehoney3@gmail.com>
2018-10-30 12:47:47 +0900

    implement encrypting password in db

--- server/models/user.js
+++ server/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const { encrypt } = require('../utils/encrypt')
 const Schema = mongoose.Schema
 
 const User = new Schema({
@@ -6,23 +7,25 @@ const User = new Schema({
     password: String
 })
 
+// don't use arrow func: no lexical scope!
 User.statics.create = function (uid, password) {
-    console.log(User)
     const user = new this({
         uid,
-        password
+        password: encrypt(password)
     })
     return user.save()
 }
 
+// statics -> for class
 User.statics.findOneByUID = function (uid) {
     return this.findOne({
         uid
     })
 }
 
+// methods -> for specific instance
 User.methods.verify = function (password) {
-    return this.password === password
+    return this.password === encrypt(password)
 }
 
 module.exports = mongoose.model('User', User)
