eaec6507dfde8b7fb274440206c6a6eab15e9d8e
makehoney <makehoney3@gmail.com>
2018-11-02 13:25:28 +0900

    todo element outline

--- client/src/components/Todo/Element.vue
+++ client/src/components/Todo/Element.vue
@@ -1,17 +1,63 @@
 <template>
     <div class="todo-element">
         <div class="el-container">
-            {{ inputData }}
+            <p class="el-title">{{ title }} [{{ calculStar }}]</p>
+            <p class="el-desc">{{ description }}</p>
+            <input type="checkbox" id="checkbox" v-model="isChecked">
+            <p class="el-deadline">마감 날짜: {{ deadline }}</p>
+            <b-btn class="el-button" size="sm">수정</b-btn>
         </div>
     </div>
 </template>
 
 <script>
+    // 동일 컴포넌트간 데이터 공유 -> vuex로 해결
+    // vue bootstrap checkbox 사용가능
     export default {
         name: 'todo-element',
+        data () {
+            return {
+                isChecked: false
+            }
+        },
+        computed: {
+            calculStar () {
+                if (this.priority === 0) return '★★★'
+                else if(this.priority === 1) return '★★'
+                else return '★'
+            }
+        },
+        methods: {
+
+        },
+        watch: {
+            isChecked () {
+                let elContainer = this.$el.querySelector('.el-container')
+                if(this.isChecked) {
+                    elContainer.style.backgroundColor = '#276DE9'
+                } else {
+                    elContainer.style.backgroundColor = '#cadbe9'
+                }
+            }
+        },
         props: {
-            inputData: {
+            title: {
                 type: String
+            },
+            description: {
+                type: String
+            },
+            priority: {
+                type: Number
+            },
+            deadline: {
+                type: String
+            },
+            isComplete: {
+                type: Boolean
+            },
+            isExpired: {
+                type: Boolean
             }
         }
     }
@@ -20,8 +66,31 @@
 <style scoped>
     .el-container {
         background-color: #cadbe9;
-        padding: 50px 0 50px 0;
+        padding: 10px 0 10px 0;
         margin: 10px auto 10px auto;
         border-radius: 5px;
+    }
+    .el-title {
+        font-weight: bold;
+        text-align: left;
+        margin: auto auto auto 10px;
+    }
+    .el-desc {
+        text-align: left;
+        margin: auto auto auto 10px;
+    }
+    .el-deadline {
+        font-size: 10px;
+        text-align: right;
+        margin: auto 30px auto auto;
+    }
+    #checkbox {
+        float: right;
+        margin: auto 10px auto auto;
+    }
+    .el-button {
+        background-color: #648dd1;
+        border-color: #648dd1;
+        width: 90%;
     }
 </style>
--- client/src/components/Todo/Main.vue
+++ client/src/components/Todo/Main.vue
@@ -1,17 +1,36 @@
 <template>
     <div class="todo-main">
         <div class="todo-container">
-            <h1 id="my-todo-title">My List</h1>
-            <div v-for="tempBuf in tempBufs">
-                <todo-element :inputData="tempBuf.inputData"></todo-element>
+            <h5 v-if="!elems.length">리스트를 등록해주세요!</h5>
+            <div v-for="elem in elems">
+                <todo-element class="toElem" :title="elem.title"
+                                             :description="elem.description"
+                                             :priority="elem.priority"
+                                             :deadline="elem.deadline"
+                                             :isComplete="elem.isComplete"
+                                             :isExpired="elem.isExpired"></todo-element>
             </div>
         </div>
         <b-btn class="form-trigger-button" @click="triggerForm()">Todo 등록</b-btn>
 
         <b-modal ref="createForm" size="lg" hide-footer title="정보 입력">
+            <!-- 입력 안된 경우 예외처리 -->
+            <!-- 모달 child component로 빼기 -->
             <form @submit.prevent="onSubmit(formData)">
-                <label>input data</label>
-                <input type="text" id="input-data" v-model="formData.inputData">
+                <label for="todo-title">제목</label>
+                <input type="text" id="todo-title" v-model="formData.title">
+                <label for="todo-desc">내용</label>
+                <textarea id="todo-desc" placeholder="상세 내용을 입력해주세요!" v-model="formData.description"></textarea>
+                <label for="todo-priority">중요도</label>
+                <div id="todo-priority">
+                    <input type="radio" id="high" value="0" v-model="formData.priority">
+                    <label for="high">★★★</label>
+                    <input type="radio" id="mid" value="1" v-model="formData.priority">
+                    <label for="mid">★★</label>
+                    <input type="radio" id="low" value="2" v-model="formData.priority">
+                    <label for="low">★</label>
+                </div>
+                <input type="date" id="todo-deadline" value="2018-01-01" v-model="formData.deadline">
                 <input type="submit" id="submit" value="완료">
             </form>
         </b-modal>
@@ -24,9 +43,15 @@
         name: 'todo-main',
         data () {
             return {
-                tempBufs: [],
+                elems: [],
                 formData: {
-                    inputData: ''
+                    _id: '',
+                    title: '',
+                    description: '',
+                    priority: 3,
+                    deadline: '',
+                    isComplete: false,
+                    isExpired: false
                 }
             }
         },
@@ -39,18 +64,34 @@
             },
             onSubmit(formData) {
                 let formInstance = Object.assign({}, formData)
-                this.tempBufs.push(formInstance)
+
+                formInstance.priority = parseInt(formInstance.priority)
+                let yyyymmdd = formInstance.deadline.split('-')
+                let intDate = (new Date(yyyymmdd[0], yyyymmdd[1] - 1, yyyymmdd[2])).getTime()
+
+                intDate > Date.now()
+                ? formInstance.isExpired = false
+                : formInstance.isExpired = true
+
+                console.log(formInstance)
+
+                // 디비 insert이후 _id값 받아서 formInstance에 저장
+                this.elems.push(formInstance)
                 this.$refs.createForm.hide()
-                this.formData.inputData = ''
+
+                // reset form
+                this.formData.title = ''
+                this.formData.description = ''
+                this.formData.priority = -1
+                this.formData.deadline = ''
+                this.isComplete = false
+                this.isExpired = false
             }
         }
     }
 </script>
 
 <style scoped>
-    #my-todo-title {
-        padding-bottom: 5%;
-    }
     .todo-container {
         /*position: relative;*/
         border-radius: 15px;
@@ -65,7 +106,25 @@
         margin: 20px auto 20px auto;
         width: 60%;
     }
-    #input-data, #submit {
+    #todo-title,
+    #todo-desc,
+    #todo-priority,
+    #submit {
         display: block;
+    }
+    .toElem {
+        transform: scale(1);
+        -webkit-transform: scale(1);
+        -moz-transform: scale(1);
+        -ms-transform: scale(1);
+        -o-transform: scale(1);
+        transition: all 0.1s ease-in-out;
+    }
+    .toElem:hover {
+        transform: scale(1.05);
+        -webkit-transform: scale(1.05);
+        -moz-transform: scale(1.05);
+        -ms-transform: scale(1.05);
+        -o-transform: scale(1.05);
     }
 </style>
