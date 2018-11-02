01efb33e8f68e0f063179d96ee2fe8970fd29567
makehoney <makehoney3@gmail.com>
2018-11-03 07:11:11 +0900

    add router for delete function

--- server/api/todo/controller.js
+++ server/api/todo/controller.js
@@ -1,4 +1,5 @@
 const Todo = require('../../models/todo')
+const mongoose = require('mongoose')
 
 exports.createTodo = async (req, res) => {
     const { formData } = req.body
@@ -23,5 +24,21 @@ exports.loadTodoList = async (req, res) => {
     const todoList = await Todo.findTodosByUID(uid)
     res.json({
         todoList
+    })
+}
+
+exports.deleteTodo = async (req, res) => {
+    let { elements } = req.body
+    for (let key in elements) {
+        try {
+            await Todo.find({_id: mongoose.Types.ObjectId(elements[key])}).remove()
+        } catch (err) {
+            res.status(409).json({
+                message: err.messgae
+            })
+        }
+    }
+    res.json({
+        message: 'success'
     })
 }
--- server/api/todo/index.js
+++ server/api/todo/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
-const { createTodo, loadTodoList } = require('./controller')
+const { createTodo, loadTodoList, deleteTodo } = require('./controller')
 const router = express.Router()
 
 router.post('/read', loadTodoList)
 router.post('/create', createTodo)
-router.post('/delete')
+router.post('/delete', deleteTodo)
 router.post('/update')
 
 module.exports = router
